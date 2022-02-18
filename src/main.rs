361357fdec8fb4bd2a981c38162eca40aed5d0c1
Florian <e.k.florian@gmail.com>
2022-02-18 22:36:51 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use btrader::trader::bTrader;
 use std::env;
 
-#[tokio::main]
-async fn main() {
-  let args: Vec<String> = env::args().collect();
-  let trader: bTrader = bTrader::new(&args[1]);
-  trader.run().await;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let trader: bTrader = bTrader::new(&args[1]);
+    trader.run();
 }
--- tests/test_config.rs
+++ tests/test_config.rs
@@ -1,20 +1,20 @@
 #[cfg(test)]
 mod config_tests {
-  #[test]
-  fn attributes_test() {
-    use btrader::config::*;
-    let cfg: Configuration = Configuration::new("config/sample_config.json");
-    assert_eq!(cfg.api_key, "your-api-key-here");
-    assert_eq!(cfg.api_secret, "your-api-secret-here");
-    assert_eq!(cfg.investment_base, "BTC");
-    assert_eq!(cfg.investment_min, 0.001);
-    assert_eq!(cfg.investment_max, 0.0015);
-    assert_eq!(cfg.investment_step, 0.0001);
-    assert_eq!(cfg.trading_enabled, false);
-    assert_eq!(cfg.trading_execution_cap, 1);
-    assert_eq!(cfg.trading_taker_fee, 0.1);
-    assert_eq!(cfg.trading_profit_threshold, 0.15);
-    assert_eq!(cfg.trading_age_threshold, 100);
-    assert_eq!(cfg.depth_size, 20);
-  }
+    #[test]
+    fn attributes_test() {
+        use btrader::config::*;
+        let cfg: Configuration = Configuration::new("config/sample_config.json");
+        assert_eq!(cfg.api_key, "your-api-key-here");
+        assert_eq!(cfg.api_secret, "your-api-secret-here");
+        assert_eq!(cfg.investment_base, "BTC");
+        assert_eq!(cfg.investment_min, 0.001);
+        assert_eq!(cfg.investment_max, 0.0015);
+        assert_eq!(cfg.investment_step, 0.0001);
+        assert_eq!(cfg.trading_enabled, false);
+        assert_eq!(cfg.trading_execution_cap, 1);
+        assert_eq!(cfg.trading_taker_fee, 0.1);
+        assert_eq!(cfg.trading_profit_threshold, 0.15);
+        assert_eq!(cfg.trading_age_threshold, 100);
+        assert_eq!(cfg.depth_size, 20);
+    }
 }
